e54c687246410c878d40aa3356e4a4a8c4e1dc56
atuk721 <8051052+atuk721@users.noreply.github.com>
2018-02-21 21:53:25 +0100

    Simplify unpack_command method.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,5 +1,6 @@
 use crc16::*;
 use redis::{Cmd, Connection, ToRedisArgs, FromRedisValue, RedisResult};
+use std::io::{BufRead, Cursor};
 use slots::SLOT_SIZE;
 
 /// Redis::Cmd's `args` field is private,
@@ -51,44 +52,14 @@ pub fn slot_for_packed_command(cmd: &[u8]) -> Option<u16> {
 /// the command's slot.
 /// This is pretty messy/can probably be better
 fn unpack_command(cmd: &[u8]) -> Vec<Vec<u8>> {
-    let mut arg: Vec<u8> = Vec::new();
     let mut args: Vec<Vec<u8>> = Vec::new();
 
-    // first 4 are some leading info ('*', len of args, '\r', '\n')
-    // the next 4 precede the first arg
-    // see: <https://github.com/mitsuhiko/redis-rs/blob/master/src/cmd.rs#L85>
-    let mut iter = cmd.iter().skip(2).peekable();
-
-    'outer: loop {
-        let b = *iter.next().unwrap();
-
-        // args are separated by 13, 10
-        if b == 13 && iter.peek().unwrap() == &&10 {
-            if arg.len() > 0 {
-                args.push(arg.clone());
-                arg.clear();
+    let cursor = Cursor::new(cmd);
+    for line in cursor.lines() {
+        if let Ok(line) = line {
+            if !line.starts_with("*") && !line.starts_with("$") {
+                args.push(line.into_bytes());
             }
-
-            // consume the next item (10)
-            iter.next();
-
-            // then, if there are more args, there should be a 36
-            // if there's nothing, we're done
-            match iter.next() {
-                Some(_) => (),
-                None => break 'outer,
-            };
-            // then the length of the args (which in theory can be any length)
-            // then another 13, 10
-            'inner: loop {
-                let b = *iter.next().unwrap();
-                if b == 13 && iter.peek().unwrap() == &&10 {
-                    iter.next();
-                    break 'inner;
-                }
-            }
-        } else {
-            arg.push(b);
         }
     }
     args
